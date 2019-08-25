1c28040d70773efc115973a075f08f37449514bb
Mohammed Suhail <48213542+MohammedSuhail98@users.noreply.github.com>
2019-08-25 19:08:09 +0530

    first commit

---
+++ src/app/player.ts
@@ -0,0 +1,8 @@
+export class PlayerClass {
+    keyCode: string;  // Unique Key Code for the player to play
+    name: string; // Name of the Player
+    color: string;  // Hex color value for player's ball
+    score : number;
+ duration : string; // Duration of the dribble in seconds 1s,0.5s
+ dribbles_left : number;
+   }
